bc624e74f000aae617805b46809509ac157b5191
Mark <markdrew53@gmail.com>
2018-02-08 11:47:58 -0600

    set generic middlewares

--- api-service/index.js
+++ api-service/index.js
@@ -1,14 +1,21 @@
+import { createServer } from 'http'
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import compression from 'compression'
+import morgan from 'morgan'
 import schema from './schema'
 import Movie from './models/Movie'
 require('dotenv').config()
 
+// create express app and http server
 const app = express()
-const PORT = parseInt(process.env.PORT, 10) || 3000
+const server = createServer(app)
+
+const PORT = process.env.PORT || 3000
+const NODE_ENV = process.env.NODE_ENV || 'development'
 
 mongoose.Promise = global.Promise;
 // Start connection
@@ -21,13 +28,22 @@ mongoose
     console.error('Failed to connect to MongoDB 😕💥')
   })
 
+// set generic middlewares
+app.use(cors());
+app.use(compression());
+app.use(morgan('common'));  
+
+// set /graphql endpoint
 app.use(
   '/graphql',
   bodyParser.json(),
   graphqlExpress({ schema, context: { Movie } })
 )
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
-app.use(cors())
+
+// set GraphiQL IDE in development
+if (NODE_ENV !== 'production') {
+  app.use(graphiqlExpress({ endpointURL: '/graphql' }))
+}
 
 app.listen(PORT, err => {
   if (err) throw err
